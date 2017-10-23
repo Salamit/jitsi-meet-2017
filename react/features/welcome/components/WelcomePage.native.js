cab6fae17c5f7a5d55de5ca4631d3c8ad12662ba
SupuniNimeshika <Supuni.Nimeshika@gmail.com>
2017-10-23 17:07:34 +0530

    changed the files as UR_Meet

--- react/features/welcome/components/WelcomePage.native.js
+++ react/features/welcome/components/WelcomePage.native.js
@@ -15,17 +15,17 @@ import styles from './styles';
 /**
  * The URL at which the privacy policy is available to the user.
  */
-const PRIVACY_URL = 'https://jitsi.org/meet/privacy';
+const PRIVACY_URL = 'https://uproom.net/privacy';
 
 /**
  * The URL at which the user may send feedback.
  */
-const SEND_FEEDBACK_URL = 'mailto:support@jitsi.org';
+const SEND_FEEDBACK_URL = 'mailto:support@uproom.net';
 
 /**
  * The URL at which the terms (of service/use) are available to the user.
  */
-const TERMS_URL = 'https://jitsi.org/meet/terms';
+const TERMS_URL = 'https://uproom.net/terms';
 
 /**
  * The native container rendering the welcome page.
