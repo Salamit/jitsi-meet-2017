9d34f65a9124404bb9510e521cbce8a88c9c0a66
sachininp <sachinipadmasiri425@gmail.com>
2017-12-07 11:58:29 +0530

    Face Tracker Positions

--- react/features/large-video/components/ObjectTracker.js
+++ react/features/large-video/components/ObjectTracker.js
@@ -7,6 +7,8 @@ import tracking from './tracking.js';
 import eye from './objects/eye';
 import face from './objects/face';
 import mouth from './objects/mouth';
+import stats from './stats.min';
+import dat from './dat.gui';
 
 // import img from '../../../../images/avatar1.png';
 // const img = require('../../../../images/avatar1.png');
@@ -21,108 +23,118 @@ import mouth from './objects/mouth';
  */
 export default class ObjectTracker extends Component {
 
-// componentDidMount(){
-//     var img = document.getElementById('img-tracker');
-//     var tracker = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
-//     tracker.setStepSize(1.7);
-//     tracking.track('#img-tracker', tracker);
-//     tracker.on('track', function(event) {
-//         event.data.forEach(function(rect) {
-//             window.plot(rect.x, rect.y, rect.width, rect.height);
-//         });
-//     });
-//     window.plot = function(x, y, w, h) {
-//         var rect = document.createElement('div');
-//         document.querySelector('.demo-container').appendChild(rect);
-//         rect.classList.add('rect');
-//         rect.style.width = w + 'px';
-//         rect.style.height = h + 'px';
-//         rect.style.left = (img.offsetLeft + x) + 'px';
-//         rect.style.top = (img.offsetTop + y) + 'px';
-//     };
-// };
-        // componentDidMount(){
-        //     console.log("sjjdsjjdjsjdsjjdsj")
-        //     console.log(eye);
-        //     // var video = document.getElementById('largeVideo');
-        //     // console.log("Video");
-        //     // console.log(video);
-        //     var img = document.getElementById('#img');
-        //     var objects = new tracking.ObjectTracker(['eye']);
-        //     objects.setStepSize(1.7);
-        //     tracking.track('#img', objects);
-        //     objects.on('track', function(event) {
-        //         event.data.forEach(function(rect) {
-        //             window.plot(rect.x, rect.y, rect.width, rect.height);
-        //             console.log(rect)
-        //         });
-        //     });
-        //
-        //     window.plot = function(x, y, w, h) {
-        //         // var element = document.getElementsByClassName("rect");
-        //         // if(element!=null){
-        //         //     element.parentNode.removeChild(element);
-        //         // }
-        //
-        //         var rect = document.createElement('div');
-        //
-        //         document.querySelector('#img').appendChild(rect);
-        //         // document.querySelector('#largeVideoWrapper').appendChild(rect);
-        //         rect.style.border = '2px solid #a64ceb';
-        //         //rect.style.left = '-1000px';
-        //         rect.style.position = 'absolute';
-        //         //rect.style.top = '-1000px';
-        //         rect.classList.add('rect');
-        //         rect.style.zIndex = '2';
-        //         rect.style.width = w + 'px';
-        //         rect.style.height = h + 'px';
-        //         rect.style.left = ( img.offsetLeft+x) + 'px';
-        //         rect.style.top = ( img.offsetTop +y) + 'px';
-        //     };
-        // }
-            componentDidMount = function() {
-                console.log("sjjdsjjdjsjdsjjdsj")
-                console.log(eye);
-                var video = document.getElementById('img-tracker');
-                console.log("Video");
-                console.log(video);
-                var objects = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
-                objects.setStepSize(1.7);
-                tracking.track('#img-tracker', objects);
-                objects.on('track', function(event) {
-                    event.data.forEach(function(rect) {
-                        window.plot(rect.x, rect.y, rect.width, rect.height);
-                    });
-                });
+    componentDidMount = function() {
+        var video = document.getElementById('largeVideo');
+        var objects = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
+        objects.setInitialScale(4);
+        objects.setStepSize(2);
+        objects.setEdgesDensity(0.1);
+        tracking.track('#largeVideo', objects,{ camera: true });
+        objects.on('track', function(event) {
+            event.data.forEach(function(rect) {
+                window.plot(rect.x, rect.y, rect.width, rect.height);
+            });
+        });
 
-                window.plot = function(x, y, w, h) {
-                    var rect = document.createElement('div');
-                    document.querySelector('.demo-container').appendChild(rect);
-                    rect.style.border = '2px solid #a64ceb';
-                    //rect.style.left = '-1000px';
-                    rect.style.position = 'absolute';
-                    //rect.style.top = '-1000px';
-                    rect.classList.add('rect');
-                    rect.style.zIndex = '2';
-                    rect.style.width = w + 'px';
-                    rect.style.height = h + 'px';
-                    rect.style.left = (video.offsetLeft + x) + 'px';
-                    rect.style.top = (video.offsetTop + y) + 'px';
-                };
-            };
+        window.plot = function(x, y, w, h) {
+            var rect = document.createElement('div');
+            document.querySelector('.demo-container').appendChild(rect);
+            rect.style.border = '2px solid #a64ceb';
+            //rect.style.left = '-1000px';
+            rect.style.position = 'absolute';
+            //rect.style.top = '-1000px';
+            rect.classList.add('rect');
+            rect.style.zIndex = '2';
+            rect.style.width = w + 'px';
+            rect.style.height = h + 'px';
+            rect.style.left = (video.offsetLeft + x) + 'px';
+            rect.style.top = (video.offsetTop + y) + 'px';
+            console.log("x="+x);
+            console.log("y="+y);
+        };
+    };
+
+//video tracker
+//             componentDidMount = function() {
+//                 console.log("sjjdsjjdjsjdsjjdsj")
+//                 console.log(eye);
+//                 var video = document.getElementById('largeVideo');
+//                 console.log("Video");
+//                 console.log(video);
+//                 var objects = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
+//                 objects.setStepSize(1.7);
+//                 tracking.track('#largeVideo', objects);
+//                 objects.on('track', function(event) {
+//                     event.data.forEach(function(rect) {
+//                         window.plot(rect.x, rect.y, rect.width, rect.height);
+//                     });
+//                 });
+//
+//                 window.plot = function(x, y, w, h) {
+//                     var rect = document.createElement('div');
+//                     document.querySelector('.demo-container').appendChild(rect);
+//                     rect.style.border = '2px solid #a64ceb';
+//                     //rect.style.left = '-1000px';
+//                     rect.style.position = 'absolute';
+//                     //rect.style.top = '-1000px';
+//                     rect.classList.add('rect');
+//                     rect.style.zIndex = '2';
+//                     rect.style.width = w + 'px';
+//                     rect.style.height = h + 'px';
+//                     rect.style.left = (video.offsetLeft + x) + 'px';
+//                     rect.style.top = (video.offsetTop + y) + 'px';
+//                     console.log("x="+x);
+//                     console.log("y="+y);
+//                 };
+//             };
+//image tracker
+//     componentDidMount = function() {
+//         console.log("sjjdsjjdjsjdsjjdsj")
+//         console.log(eye);
+//         var image = document.getElementById('img-tracker');
+//         var objects = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
+//         objects.setStepSize(1.7);
+//         tracking.track('#img-tracker', objects);
+//         objects.on('track', function(event) {
+//             console.log("trackingObjects");
+//             event.data.forEach(function(rect) {
+//                 window.plot(rect.x, rect.y, rect.width, rect.height);
+//             });
+//         });
+//
+//         window.plot = function(x, y, w, h) {
+//             var rect = document.createElement('div');
+//             document.querySelector('.demo-container').appendChild(rect);
+//             rect.style.border = '2px solid #a64ceb';
+//             rect.style.backgroundColor = 'blue';
+//             //rect.style.left = '-1000px';
+//             rect.style.position = 'absolute';
+//             //rect.style.top = '-1000px';
+//             rect.classList.add('rect');
+//             rect.style.zIndex = '5';
+//             rect.style.width = w + 'px';
+//             rect.style.height = h + 'px';
+//             rect.style.left = (image.offsetLeft + x) + 'px';
+//             rect.style.top = (image.offsetTop + y) + 'px';
+//             console.log("x="+x);
+//             console.log("y"+y);
+//         };
+//     };
             render(){
                 return(
-                    <div id = 'largeVideoWrapper'>
-                        <img id="img-tracker"
-                        src="https://www.clarabridge.com/wp-content/uploads/2014/11/customersentiment_face5.png"/>
-                        <video
-                        autoPlay = { true }
-                        id = 'largeVideo'
-                        muted = { true }
-                        style = {{ zIndex: 1 }}>
-                        </video>
-                        {/*{this.componentDidMount()}*/}
-                        <div class="demo-container"></div>
+                    <div class="demo-container">
+                        <div id = 'largeVideoWrapper'>
+                            <br/><br/>
+                            {/*<img id="img-tracker"*/}
+                            {/*src="https://www.clarabridge.com/wp-content/uploads/2014/11/customersentiment_face5.png"/>*/}
+                            <video
+                                autoPlay = { true }
+                                id = 'largeVideo'
+                                muted = { true }
+                                style = {{ zIndex: 1 }}>
+                            </video>
+                            {/*{this.componentDidMount()}*/}
+                        </div>
                     </div>
                 );
             }
