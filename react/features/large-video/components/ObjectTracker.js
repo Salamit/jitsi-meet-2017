8a6e1e12b6e80dfd3edfc639b0a1e89b1d53e099
sachininp <sachinipadmasiri425@gmail.com>
2017-12-08 13:52:09 +0530

    Face Tracker with Clearing Previously Tracked Rectangles

--- react/features/large-video/components/ObjectTracker.js
+++ react/features/large-video/components/ObjectTracker.js
@@ -22,38 +22,42 @@ import dat from './dat.gui';
  * @extends Component
  */
 export default class ObjectTracker extends Component {
+//video tracker 1
+//     componentDidMount = function() {
+//         var video = document.getElementById('largeVideo');
+//         var objects = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
+//         objects.setInitialScale(4);
+//         objects.setStepSize(2);
+//         objects.setEdgesDensity(0.1);
+//         tracking.track('#largeVideo', objects);
+//         objects.on('track', function(event) {
+//
+//             event.data.forEach(function(rect) {
+//                 window.plot(rect.x, rect.y, rect.width, rect.height);
+//             });
+//         });
+//
+//                 window.plot = function(x, y, w, h) {
+//                     var rect = document.createElement('div');
+//                     document.querySelector('.demo-container').appendChild(rect);
+//                     rect.style.border = '2px solid #a64ceb';
+//                     rect.style.position = 'absolute';
+//                     rect.classList.add('rect');
+//                     rect.style.zIndex = '2';
+//                     rect.style.width = w + 'px';
+//                     rect.style.height = h + 'px';
+//                     rect.style.left = (video.offsetLeft + y) + 'px';
+//                     rect.style.top = (video.offsetTop + x) + 'px';
+//                     console.log("x="+x);
+//                     console.log("y="+y);
+//                 };
+//         var gui = new dat.GUI();
+//         gui.add(objects, 'edgesDensity', 0.1, 0.5).step(0.01);
+//         gui.add(objects, 'initialScale', 1.0, 10.0).step(0.1);
+//         gui.add(objects, 'stepSize', 1, 5).step(0.1);
+//     };
 
-    componentDidMount = function() {
-        var video = document.getElementById('largeVideo');
-        var objects = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
-        objects.setInitialScale(4);
-        objects.setStepSize(2);
-        objects.setEdgesDensity(0.1);
-        tracking.track('#largeVideo', objects);
-        objects.on('track', function(event) {
-
-            event.data.forEach(function(rect) {
-                window.plot(rect.x, rect.y, rect.width, rect.height);
-            });
-        });
-
-                window.plot = function(x, y, w, h) {
-                    var rect = document.createElement('div');
-                    document.querySelector('.demo-container').appendChild(rect);
-                    rect.style.border = '2px solid #a64ceb';
-                    rect.style.position = 'absolute';
-                    rect.classList.add('rect');
-                    rect.style.zIndex = '2';
-                    rect.style.width = w + 'px';
-                    rect.style.height = h + 'px';
-                    rect.style.left =  y + 'px';
-                    rect.style.top = x + 'px';
-                    console.log("x="+x);
-                    console.log("y="+y);
-                };
-    };
-
-//video tracker
+//video tracker 2
 //             componentDidMount = function() {
 //                 console.log("sjjdsjjdjsjdsjjdsj")
 //                 console.log(eye);
@@ -86,6 +90,41 @@ export default class ObjectTracker extends Component {
 //                     console.log("y="+y);
 //                 };
 //             };
+
+// video tracker 3
+    componentDidMount = function() {
+        var video = document.getElementById('largeVideo');
+        var canvas = document.getElementById('canvas');
+        var context = canvas.getContext('2d');
+
+        var tracker = new tracking.ObjectTracker('face');
+        tracker.setInitialScale(4);
+        tracker.setStepSize(2);
+        tracker.setEdgesDensity(0.1);
+
+        tracking.track('#largeVideo', tracker, { camera: true });
+
+        tracker.on('track', function(event) {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+
+            event.data.forEach(function(rect) {
+                context.strokeStyle = 'red';
+                context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+                context.font = '8px Helvetica';
+                context.fillStyle = "#fff";
+                context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+                context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+                console.log("x="+rect.x);
+                console.log("y="+rect.y);
+            });
+        });
+
+        var gui = new dat.GUI();
+        gui.add(tracker, 'edgesDensity', 0.1, 0.5).step(0.01);
+        gui.add(tracker, 'initialScale', 1.0, 10.0).step(0.1);
+        gui.add(tracker, 'stepSize', 1, 5).step(0.1);
+    };
+
 //image tracker
 //     componentDidMount = function() {
 //         console.log("sjjdsjjdjsjdsjjdsj")
@@ -132,7 +171,7 @@ export default class ObjectTracker extends Component {
                                 muted = { true }
                                 style = {{ zIndex: 1 }}>
                             </video>
-                            {/*{this.componentDidMount()}*/}
+                            <canvas id="canvas"></canvas>
                         </div>
                     </div>
                 );
